0e6e6db2cac744ed6cd1213df824f8a4eac775d9
arakhm6 <arakhm6@gmail.com>
2021-02-26 23:03:08 -0500

    coloring input menu

--- app.js
+++ app.js
@@ -21,6 +21,14 @@ function randomColors()
         div.style.backgroundColor = randomColor;
         hexText.innerText = randomColor;
         checkTextContrast(randomColor, hexText);
+
+        const color = chroma(randomColor);
+        const sliders = div.querySelectorAll('.sliders input');
+        const hue = sliders[0];
+        const brightness = sliders[1];
+        const saturation = sliders[2];
+
+        colorizeSliders(color, hue, brightness, saturation);
     });
 }
 
@@ -36,6 +44,20 @@ function checkTextContrast(color, text)
         text.style.color = 'white';
     }
     
+}
+
+function colorizeSliders(color, hue, brightness, saturation)
+{
+    const midBrightness = color.set('hsl.l', 0.5);
+    const scaleBrightness = chroma.scale(['black', midBrightness, 'white']);
+
+    const noSaturation = color.set('hsl.s', 0);
+    const fullSaturation = color.set('hsl.s', 1);
+    const scaleSaturation = chroma.scale([noSaturation, color, fullSaturation]);
+
+    hue.style.backgroundImage = `linear-gradient(to right, rgb(204, 75, 75), rgb(204, 204, 75), rgb(75, 204, 75), rgb(75, 204, 204), rgb(75, 75, 204), rgb(204, 75, 204), rgb(204, 75, 75))`;
+    brightness.style.backgroundImage = `linear-gradient(to right, ${scaleBrightness(0)}, ${scaleBrightness(0.5)}, ${scaleBrightness(1)})`;
+    saturation.style.backgroundImage = `linear-gradient(to right, ${scaleSaturation(0)}, ${scaleSaturation(1)})`;
 }
 
 randomColors();
